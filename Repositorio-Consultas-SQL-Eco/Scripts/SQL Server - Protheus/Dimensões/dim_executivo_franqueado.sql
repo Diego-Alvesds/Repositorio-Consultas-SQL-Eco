04f1f6053bbc359275177ca1a491cd81e627371a
diego.santos <diegoalvesds@outlook.com>
2026-01-14 02:41:44 -0300

    new tab

---
+++ Repositorio-Consultas-SQL-Eco/Scripts/SQL Server - Protheus/Dimensões/dim_executivo_franqueado.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT 
+	A3_COD AS cod_vendedor2,
+	A3_NOME AS nome_vendedor2,
+	A3_NREDUZ AS nome_vend2_reduzido,
+	MAX(DATA_ATUALIZACAO) AS data_stamp
+FROM SA3010
+WHERE 1 = 1
+GROUP BY A3_COD,
+		 A3_NOME, 
+		 A3_NREDUZ
+		 
