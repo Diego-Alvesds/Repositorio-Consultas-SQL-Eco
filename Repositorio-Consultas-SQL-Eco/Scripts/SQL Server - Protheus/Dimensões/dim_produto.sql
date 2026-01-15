47a71a0f64dc5a71ac7fc8c51c9727bd51ae7690
diego.santos <diegoalvesds@outlook.com>
2026-01-15 02:30:02 -0300

    add new dim

---
+++ Repositorio-Consultas-SQL-Eco/Scripts/SQL Server - Protheus/Dimensões/dim_produto.sql
@@ -0,0 +1,10 @@
+SELECT 
+	B1_COD AS cod_produto,
+	B1_DESC AS desc_produto,
+	B1_TIPO AS tipo_produto,
+	B1_UM AS unidade_medida,
+	B1_LOCPAD AS armazem,
+	B1_GRUPO AS grupo,
+	DATA_ATUALIZACAO AS data_stamp
+FROM SB1010
+WHERE 1 = 1
