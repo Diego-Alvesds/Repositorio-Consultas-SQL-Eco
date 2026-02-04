503d6f8c83dd38f2d9bfecb9f3d9fb1d49c3655e
diego.santos <diegoalvesds@outlook.com>
2026-02-04 11:59:31 -0300

    add fato_produto_adicional

---
+++ Repositorio-Consultas-SQL-Eco/Scripts/My SQL - Fluig/Fatos/fato_produto_adicional.sql
@@ -0,0 +1,46 @@
+SELECT
+	m30.proposta AS PROPOSTA,
+	m30.entradaImputado AS filial,
+    m30.contrato_protheus AS contrato,
+   	CONVERT(m30.dataCriacao, DATE) AS data_criacao,
+    CONVERT(m30.dataAtualizacao, DATE) AS data_atualizacao,
+    m30.atividadeDesc AS status_projeto,
+    CONVERT(m30.tipoTrocaSimulacao, DATE) AS data_negociacao,
+    m30.tipoSimulacaoDesc AS tipo_simulacao,
+    m30.descricaoUnidade AS unidade,
+    m30.cidadeInstalacaoDesc AS cidade,
+    m30.estadoInstalacao AS estado,
+    CASE 
+        WHEN m30.estadoInstalacao IN ('AC','AM','AP','PA','RO','RR','TO') THEN 'Norte'
+        WHEN m30.estadoInstalacao IN ('AL','BA','CE','MA','PB','PE','PI','RN','SE') THEN 'Nordeste'
+        WHEN m30.estadoInstalacao IN ('DF','GO','MT','MS') THEN 'Centro-Oeste'
+        WHEN m30.estadoInstalacao IN ('ES','MG','RJ','SP') THEN 'Sudeste'
+        WHEN m30.estadoInstalacao IN ('PR','RS','SC') THEN 'Sul'
+    ELSE 'Região não identificada'
+    END AS regiao,
+    CAST(m30.potenciaSimul AS DECIMAL (10,2)) AS potencia,
+    CAST(REPLACE(REPLACE(REGEXP_REPLACE(m30.valorASerPago, '[^0-9,.]', ''),'.',''),',','.') AS DECIMAL(10,2)) AS valor_final,
+	m55.codigoProdutoAdicional AS cod_produto_adicional,
+    m55.produtosAdicionaisDesc AS produto_adicional,
+	m55.qtdProdutosAdicionais AS qtd_produto_adicional,
+	CAST(REPLACE(REPLACE(REGEXP_REPLACE(m55.valorUnitProdutosAdicionais, '[^0-9,.]', ''),'.',''),',','.') AS DECIMAL(10,2)) AS valor_unit_produto_adicional,
+	CAST(REPLACE(REPLACE(REGEXP_REPLACE(m55.valorProdutosAdicionais, '[^0-9,.]', ''),'.',''),',','.') AS DECIMAL(10,2)) AS subtotal_produto_adicional,
+	CAST(REPLACE(REPLACE(REGEXP_REPLACE(m30.totalProdutosAdicionais, '[^0-9,.]', ''),'.',''),',','.') AS DECIMAL(10,2)) AS total_produtos_adicionais
+FROM DOCUMENTO d
+JOIN ML001030 m30 ON
+	m30.companyid = d.COD_EMPRESA
+	AND m30.documentid = d.NR_DOCUMENTO
+	AND m30.version = d.NR_VERSAO
+JOIN ML001055 m55 ON
+	m55.companyid = m30.companyid
+	AND m55.documentid = m30.documentid
+	AND m55.version = m30.version
+WHERE
+	1 = 1
+	AND d.COD_EMPRESA = 1
+	AND d.COD_LISTA = 30
+	AND d.VERSAO_ATIVA = 1
+	AND m30.atividadeDesc <> ''
+	AND m30.proposta <> '00000NaN'
+	AND m30.atividade <> '15'
+	AND m30.dataCriacao BETWEEN '2024-01-01 00:00:00' AND '2026-12-31 23:59:59'
