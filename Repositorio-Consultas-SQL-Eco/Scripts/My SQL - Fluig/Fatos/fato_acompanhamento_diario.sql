1f8bcf8fc30c3d351c9ffcd5fe53aad0b7200fa4
diego.santos <diegoalvesds@outlook.com>
2026-02-20 09:32:27 -0300

    att fato_acompanhamento_diario

--- Repositorio-Consultas-SQL-Eco/Scripts/My SQL - Fluig/Fatos/fato_acompanhamento_diario.sql
+++ Repositorio-Consultas-SQL-Eco/Scripts/My SQL - Fluig/Fatos/fato_acompanhamento_diario.sql
@@ -26,5 +26,5 @@ WHERE 1 = 1
     AND d.COD_LISTA = 30
     AND d.VERSAO_ATIVA = 1
     AND m30.atividadeDesc <> ''
-    AND m30.atividade <> '15'
+    AND m30.atividade <> '15' 
     AND m30.dataAtualizacao BETWEEN '2025-01-01 00:00:00' AND '2026-12-31 23:59:59'
