8ea821b862e844658a055295d58c66a9efd7253b
diego.santos <diegoalvesds@outlook.com>
2026-03-05 08:15:26 -0300

    att fato_historico_go_temp

--- Repositorio-Consultas-SQL-Eco/Scripts/My SQL - Fluig/Fatos/fato_historico_go_temp.sql
+++ Repositorio-Consultas-SQL-Eco/Scripts/My SQL - Fluig/Fatos/fato_historico_go_temp.sql
@@ -1,21 +1,28 @@
  SELECT 
-    proposta AS PROPOSTA,
-    dataHistoricoCompleta AS DATA_HISTORICO_STATUS,
-    statusHistoricoDe AS HISTORICO_STATUS_DE,
-    statusHistorico AS HISTORICO_STATUS_PARA,
-    nomeUsuarioHistorico AS USUARIO_HISTORICO_STATUS,
-    matriculaUsuarioHistorico AS MATRICULA_HISTORICO_STATUS,
+    m30.proposta AS PROPOSTA,
+    m30.contrato_protheus AS CONTRATO,
+    m30.nomeRazaoCli AS CLIENTE,
+    m30.descricaoUnidade AS UNIDADE,
+    CAST(m30.potenciaSimul AS DECIMAL (10,2)) AS POTENCIA,
+    CAST(REPLACE(REPLACE(REGEXP_REPLACE(m30.valorASerPago, '[^0-9,.]', ''),'.',''),',','.') AS DECIMAL(10,2)) AS VALOR_FINAL,
+    m112.dataHistoricoCompleta AS DATA_HISTORICO_STATUS,
+    m112.statusHistoricoDe AS HISTORICO_STATUS_DE,
+    m112.statusHistorico AS HISTORICO_STATUS_PARA,
+    m112.nomeUsuarioHistorico AS USUARIO_HISTORICO_STATUS,
+    m112.matriculaUsuarioHistorico AS MATRICULA_HISTORICO_STATUS,
     NOW() AS data_stamp
 FROM 
     ML001030 m30
-    JOIN DOCUMENTO d ON d.COD_REG_LISTA = m30.ID
-    JOIN ML001112 m112 ON m112.companyid = m30.companyid
+    JOIN DOCUMENTO d 
+    ON d.COD_REG_LISTA = m30.ID
+    JOIN ML001112 m112 
+    ON m112.companyid = m30.companyid
     AND m112.masterid = m30.ID
 WHERE 
     d.COD_EMPRESA = 1 
     AND d.COD_LISTA = 30
     AND d.VERSAO_ATIVA = 1
-    AND m30.dataAtualizacao BETWEEN '2025-08-01 00:00:00' AND '2026-12-31 23:59:59'
+    AND m30.dataAtualizacao BETWEEN '2025-10-01 00:00:00' AND '2026-12-31 23:59:59'
     AND m30.proposta <> ''
     AND m30.proposta <> "00000NaN"
     AND m30.proposta IS NOT NULL
