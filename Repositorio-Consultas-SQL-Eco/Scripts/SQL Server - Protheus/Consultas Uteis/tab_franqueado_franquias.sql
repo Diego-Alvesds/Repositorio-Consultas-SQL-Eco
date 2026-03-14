dd99bccd5be2716c09935b6b1e081e6ebf8a7ab0
diego.santos <diegoalvesds@outlook.com>
2026-03-14 18:11:16 -0300

    att tabs

---
+++ Repositorio-Consultas-SQL-Eco/Scripts/SQL Server - Protheus/Consultas Uteis/tab_franqueado_franquias.sql
@@ -0,0 +1,20 @@
+SELECT DISTINCT 
+	A3_COD AS cod_vendedor2,
+	A3_NOME AS nome_vendedor2,
+	A3_NREDUZ AS nome_vend2_reduzido,
+	NULLIF(CONVERT(DATE, A3_ADMISS), '1900-01-01') AS data_admissao,
+	CONVERT(FLOAT,A3_COMIS) AS taxa_comissao,
+	CASE 
+		WHEN A3_TIPO = 'E' THEN 'EXTERNO'
+		WHEN A3_TIPO = 'I' THEN 'INTERNO'
+		WHEN A3_TIPO = 'P' THEN 'PARCEIRO'
+	END AS tipo_vendedor,
+	MAX(DATA_ATUALIZACAO) AS data_stamp
+FROM SA3010
+WHERE 1 = 1
+GROUP BY A3_COD,
+		 A3_NOME, 
+		 A3_NREDUZ,
+		 A3_ADMISS,
+		 A3_COMIS,
+		 A3_TIPO
