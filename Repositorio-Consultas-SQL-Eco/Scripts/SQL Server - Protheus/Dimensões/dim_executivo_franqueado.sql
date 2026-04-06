92a4cab339a301fe8e679e034588ded6e623c838
diego.santos <diegoalvesds@outlook.com>
2026-04-06 17:48:43 -0300

    att dim_executivo_franqueado

--- Repositorio-Consultas-SQL-Eco/Scripts/SQL Server - Protheus/Dimensões/dim_executivo_franqueado.sql
+++ Repositorio-Consultas-SQL-Eco/Scripts/SQL Server - Protheus/Dimensões/dim_executivo_franqueado.sql
@@ -3,11 +3,13 @@ SELECT DISTINCT
 	A3_NOME AS nome_vendedor2,
 	A3_NREDUZ AS nome_vend2_reduzido,
 	NULLIF(CONVERT(DATE, A3_ADMISS), '1900-01-01') AS data_admissao,
+	A3_TIPO AS tipo,
 	MAX(DATA_ATUALIZACAO) AS data_stamp
 FROM SA3010
 WHERE 1 = 1
 GROUP BY A3_COD,
 		 A3_NOME, 
 		 A3_NREDUZ,
-		 A3_ADMISS
+		 A3_ADMISS,
+		 A3_TIPO
 		 
