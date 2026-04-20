28e4bdeb6ae3c77237995ba0c5b973718d6a645a
diego.santos <diegoalvesds@outlook.com>
2026-04-20 14:31:07 -0300

    att fato_promocao

--- Repositorio-Consultas-SQL-Eco/Scripts/My SQL - Fluig/Fatos/fato_promocao.sql
+++ Repositorio-Consultas-SQL-Eco/Scripts/My SQL - Fluig/Fatos/fato_promocao.sql
@@ -61,7 +61,7 @@ WHERE 1 = 1
     AND d.COD_LISTA = 30
     AND d.VERSAO_ATIVA = 1
     AND m36.promocaoEscolhida = 'promocaoEscolhida'
-    AND m30.dataAtualizacao BETWEEN '2025-01-01 00:00:00' AND '2026-12-31 23:59:59'
+    AND m30.dataAtualizacao BETWEEN '2026-01-01 00:00:00' AND '2026-12-31 23:59:59'
     AND m30.atividadeDesc <> ''
     AND m30.proposta <> '00000NaN'
     AND m30.atividade <> '15'
