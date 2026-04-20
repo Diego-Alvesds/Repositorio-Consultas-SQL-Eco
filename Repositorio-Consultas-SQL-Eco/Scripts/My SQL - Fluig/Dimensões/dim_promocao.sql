d9aeb4bf6a5ecc66a3d9e63cd5e7c9973efe5131
diego.santos <diegoalvesds@outlook.com>
2026-04-20 16:05:50 -0300

    ATT dim_promocao

--- Repositorio-Consultas-SQL-Eco/Scripts/My SQL - Fluig/Dimensões/dim_promocao.sql
+++ Repositorio-Consultas-SQL-Eco/Scripts/My SQL - Fluig/Dimensões/dim_promocao.sql
@@ -1,31 +1,31 @@
 WITH ultima_promo AS(
-	SELECT 
-		documentid,
+    SELECT 
+        documentid,
         MAX(version) AS max_version
     FROM ML001015
     WHERE companyid = 1
     GROUP BY documentid
 )
 SELECT 
-	m15.idPromocaoCriada AS id_promocao_criada,
-	m15.descPromocao AS desc_promocao,
-	m15.potencia AS potencia_promocao,
-	m15.qtdModulo AS qtd_modulo,
-	m15.valorPadrao AS valor_deconto,
-	m15.valorTabela AS valor_tabela,
-	m15.moduloDesc AS desc_modulo_promocao,
-	m15.inversorDesc AS desc_inversor,
-	m15.permissaoParaSimular AS permissao_simular,
-	m15.tipoPromocao AS cod_tipo_promocao,
-	m15.tipoPromocaoDesc AS desc_tipo_promocao,
-	m15.codigoProdutoPromocao AS cod_produto_promocao,
-	CASE 
-		WHEN m15.optStatusTXT = 'true' THEN 'Sim'
-		WHEN m15.optStatusTXT = 'false' THEN 'Não'
-	END AS status_promocao
+    m15.idPromocaoCriada AS id_promocao_criada,
+    m15.descPromocao AS desc_promocao,
+    CAST(m15.potencia AS DECIMAL (10,2)) AS potencia_promocao,
+    CAST(m15.qtdModulo AS UNSIGNED INTEGER) AS qtd_modulo,
+    CAST(REPLACE(REPLACE(REGEXP_REPLACE(m15.valorPadrao, '[^0-9,.]', ''),'.',''),',','.') AS DECIMAL(10,2)) AS valor_deconto,
+    CAST(REPLACE(REPLACE(REGEXP_REPLACE(m15.valorTabela , '[^0-9,.]', ''),'.',''),',','.') AS DECIMAL(10,2)) AS valor_tabela,
+    m15.moduloDesc AS desc_modulo_promocao,
+    m15.inversorDesc AS desc_inversor,
+    m15.permissaoParaSimular AS permissao_simular,
+    m15.tipoPromocao AS cod_tipo_promocao,
+    m15.tipoPromocaoDesc AS desc_tipo_promocao,
+    m15.codigoProdutoPromocao AS cod_produto_promocao,
+    CASE 
+        WHEN m15.optStatusTXT = 'true' THEN 'Sim'
+        WHEN m15.optStatusTXT = 'false' THEN 'Não'
+    END AS status_promocao
 FROM  ML001015 m15
 INNER JOIN ultima_promo up
-	ON up.documentid = m15.documentid
-	AND m15.version = up.max_version
+    ON up.documentid = m15.documentid
+    AND m15.version = up.max_version
 WHERE 1 = 1
 AND m15.companyid = 1
