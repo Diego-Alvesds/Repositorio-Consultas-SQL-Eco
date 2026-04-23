49fdd181a669611fabd5c71e9aacd1fedc4eaacd
diego.santos <diegoalvesds@outlook.com>
2026-04-23 01:33:15 -0300

    add fato_rota_comercial

---
+++ Repositorio-Consultas-SQL-Eco/Scripts/My SQL - Fluig/Fatos/fato_rota_comercial.sql
@@ -0,0 +1,43 @@
+SELECT
+    m30.proposta AS proposta,
+    m30.entradaImputado AS filial,
+    m30.contrato_protheus AS contrato,
+    TRIM(CONCAT(m30.entradaImputado,contrato_protheus)) AS filialcontrato,
+    CONVERT(m30.dataCriacao, DATE) AS data_criacao,
+    CONVERT(m30.dataAtualizacao, DATE) AS data_atualizacao,
+    m30.atividade AS cod_status,
+    m30.atividadeDesc AS status_projeto,
+    m30.tipoFinanciamentoDesc AS pf_pj,
+    m30.tipoSimulacaoDesc AS tipo_simulacao,
+    m30.nomeRazaoCli AS cliente,
+    m30.documentoIdent AS documento,
+    m30.cidadeInstalacao AS cod_cidade_instalacao,
+    m30.cidadeInstalacaoDesc AS cidade,
+    m30.estadoInstalacao AS estado,
+    m30.codigoUnidade AS cod_unidade,
+    m30.descricaoUnidade AS unidade,
+    m30.classe AS classe,
+    CAST(m30.potenciaSimul AS DECIMAL (10,2)) AS potencia,
+    CAST(REPLACE(REPLACE(REGEXP_REPLACE(m30.valorASerPago, '[^0-9,.]', ''),'.',''),',','.') AS DECIMAL(10,2)) AS valor_final, 
+    m30.vendedor_protheus AS cod_vendedor_franqueado,
+    m30.executivo_protheus AS cod_vendedor_executivo,      
+    CONVERT(m30.tipoTrocaSimulacao, DATE) AS data_negociacao,
+    CASE
+        WHEN TRIM(IFNULL(m30.marcaInversorPromocaoDesc, '')) = TRIM(IFNULL(m30.marcaInversorDesc, '')) AND TRIM(IFNULL(m30.marcaInversorDesc, '')) <> '' THEN TRIM(m30.marcaInversorDesc)
+        WHEN TRIM(IFNULL(m30.marcaInversorPromocaoDesc, '')) = '' AND TRIM(IFNULL(m30.marcaInversorDesc, '')) <> '' THEN TRIM(m30.marcaInversorDesc)
+        WHEN TRIM(IFNULL(m30.marcaInversorDesc, '')) = '' AND TRIM(IFNULL(m30.marcaInversorPromocaoDesc, '')) <> '' THEN TRIM(m30.marcaInversorPromocaoDesc)
+        WHEN TRIM(IFNULL(m30.marcaInversorPromocaoDesc, '')) <> TRIM(IFNULL(m30.marcaInversorDesc, '')) AND TRIM(IFNULL(m30.marcaInversorDesc, '')) <> '' THEN TRIM(m30.marcaInversorDesc)
+        ELSE 'Venda Avulsa'
+    END AS tipo_inversor,
+    NOW() AS data_stamp
+FROM ML001030 m30
+JOIN DOCUMENTO d 
+ON d.COD_REG_LISTA = m30.ID
+WHERE 1 = 1
+    AND d.COD_EMPRESA = 1 
+    AND d.COD_LISTA = 30
+    AND d.VERSAO_ATIVA = 1
+    AND m30.dataCriacao BETWEEN '2022-01-01 00:00:00' AND '2026-12-31 23:59:59'
+    AND m30.atividadeDesc <> ''
+    AND m30.proposta <> '00000NaN'
+    AND m30.atividadeDesc <> 'Finalizar Perdida'
