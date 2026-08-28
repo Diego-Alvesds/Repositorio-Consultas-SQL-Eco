511dd800c301ae7473fac2b394c302f7385a30c2
Diego-Alvesds <diegoalvesds@outlook.com>
2026-08-28 11:55:46 -0300

    att dim_centro_custo

--- Repositorio-Consultas-SQL-Eco/Scripts/SQL Server - Protheus/Dimensões/dim_centro_custo.sql
+++ Repositorio-Consultas-SQL-Eco/Scripts/SQL Server - Protheus/Dimensões/dim_centro_custo.sql
@@ -3,5 +3,7 @@ SELECT
 	CTT_CUSTO AS centro_custo,
 	CTT_CLASSE AS classe,
 	CTT_DESC01 AS descricao_centro_custo,
+	CTT_CCSUP AS cc_superior,
 	DATA_ATUALIZACAO AS data_stamp
 FROM CTT010
+WHERE 1 = 1
