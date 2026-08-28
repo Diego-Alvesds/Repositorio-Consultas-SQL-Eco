d79e91be3505203b1d23090a8d57ae9e094dc760
Diego-Alvesds <diegoalvesds@outlook.com>
2026-08-28 15:08:44 -0300

    att dim_cond_pagamento

--- Repositorio-Consultas-SQL-Eco/Scripts/SQL Server - Protheus/Dimensões/dim_cond_pagamento.sql
+++ Repositorio-Consultas-SQL-Eco/Scripts/SQL Server - Protheus/Dimensões/dim_cond_pagamento.sql
@@ -5,6 +5,7 @@ SELECT DISTINCT
 	CONCAT(E4_FILIAL, E4_CODIGO) AS filial_codpag,
 	E4_COND AS condicao_pagamento,
 	E4_DESCRI AS descricao_cond_pagamento,
-	E4_FORMA AS forma_pagamento
+	E4_FORMA AS forma_pagamento,
+	DATA_ATUALIZACAO AS data_stamp
 FROM SE4010
 WHERE 1 = 1
