2bf965531bfb3d6b1d9928f218cb848294ca7850
Diego-Alvesds <diegoalvesds@outlook.com>
2026-08-28 14:14:36 -0300

    att dim_centro_custo

--- Repositorio-Consultas-SQL-Eco/Scripts/SQL Server - Protheus/Dimensões/dim_centro_custo.sql
+++ Repositorio-Consultas-SQL-Eco/Scripts/SQL Server - Protheus/Dimensões/dim_centro_custo.sql
@@ -1,6 +1,7 @@
 SELECT 
 	CTT_FILIAL AS filial,
 	CTT_CUSTO AS centro_custo,
+	TRIM(CONCAT(TRIM(CTT_FILIAL),TRIM(CTT_CUSTO))) AS filial_cc,
 	CTT_CLASSE AS classe,
 	CTT_DESC01 AS descricao_centro_custo,
 	CTT_CCSUP AS cc_superior,
