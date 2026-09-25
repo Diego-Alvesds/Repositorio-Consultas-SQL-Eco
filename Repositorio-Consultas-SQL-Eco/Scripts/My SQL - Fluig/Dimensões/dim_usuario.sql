176a42e99a8919642081fed0d32d591abac08149
Diego-Alvesds <diegoalvesds@outlook.com>
2026-09-25 10:54:31 -0300

    att dim_usuario

--- Repositorio-Consultas-SQL-Eco/Scripts/My SQL - Fluig/Dimensões/dim_usuario.sql
+++ Repositorio-Consultas-SQL-Eco/Scripts/My SQL - Fluig/Dimensões/dim_usuario.sql
@@ -1,8 +1,12 @@
 SELECT
-	FULL_NAME AS nome_usuario,
-	EMAIL AS email,
-	USER_CODE AS cod_usuario,
-	ut.LOGIN AS login
+	u.FULL_NAME AS nome_usuario,
+	ut.EMAIL AS email,
+	ut.USER_CODE AS cod_usuario,
+	ut.LOGIN AS login,
+	CASE 
+		WHEN ut.USER_STATE = 1 THEN "Ativo"
+		WHEN ut.USER_STATE = 2 THEN "Inativo"
+	END AS status
 FROM
 	FDN_USERTENANT AS ut
 INNER JOIN FDN_USER u ON
